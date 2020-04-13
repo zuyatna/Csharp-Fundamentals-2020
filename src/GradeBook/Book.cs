a7f86ecaa6039084a9849175d995933808d17ffa
unknown <zuyatna@gmail.com>
2020-04-13 15:37:58 +0700

    58 - Pattern matching with switch

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -11,9 +11,42 @@ namespace GradeBook
             Name = name;
         }
 
+        public void AddLetterGrade(char letter)
+        {
+            switch (letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+
+                case 'B':
+                    AddGrade(80);
+                    break;
+
+                case 'C':
+                    AddGrade(70);
+                    break;
+
+                case 'D':
+                    AddGrade(60);
+                    break;
+
+                default:
+                    AddGrade(0);
+                    break;
+            }
+        }
+
         public void AddGrade(double grade)
         {
-            grades.Add(grade);
+            if (grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+            }
+            else
+            {
+                System.Console.WriteLine("Invalid value!");
+            }
         }
 
         public Statistics ShowStatistics()
@@ -32,6 +65,29 @@ namespace GradeBook
             
             result.Average /= grades.Count; 
             
+            switch (result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+
+                default:
+                    result.Letter = 'E';
+                    break;
+            }
+
             return result;           
         }
 
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,17 +7,27 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {            
-            var book = new Book("Suyatna book");
+            var book = new Book("Suyatna book");            
 
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.3);
-            book.ShowStatistics();  
+            while (true)
+            {
+                System.Console.WriteLine("Enter a grade or 'q' to quite");
+                var input = Console.ReadLine();
+
+                if (input == "q")
+                {
+                    break;
+                }
+
+                var grade = double.Parse(input);
+                book.AddGrade(grade);
+            }                
 
             var statistics = book.ShowStatistics();
             System.Console.WriteLine($"The average grade is {statistics.Average:N2}"); // N2 = dua nol dibelakang koma
             System.Console.WriteLine($"The highest grade is {statistics.High}");
             System.Console.WriteLine($"The lowest grade is {statistics.Low}");
+            System.Console.WriteLine($"The letter grade is {statistics.Letter}");
         }
     }
 }
