02e7e77fa854ac8dcfca2a7d762cb8c364e2d76f
unknown <zuyatna@gmail.com>
2020-04-22 07:27:25 +0700

    87 - Refactoring Statistics

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -62,7 +62,21 @@ namespace GradeBook
 
         public override Statistics GetStatistics()
         {
-            throw new NotImplementedException();
+            var result = new Statistics();
+
+            using(var reader = File.OpenText($"{Name}.txt"))
+            {
+                var line = reader.ReadLine();
+
+                while (line != null)
+                {
+                    var number = double.Parse(line);                    
+                    result.Add(number);
+                    line = reader.ReadLine();
+                }
+            }
+
+            return result;
         }
     }
 
@@ -129,42 +143,12 @@ namespace GradeBook
 
         public override Statistics GetStatistics()
         {
-            var result = new Statistics();
-            result.Average = 0.0;
-            result.High = double.MinValue;
-            result.Low = double.MaxValue;
+            var result = new Statistics();            
 
-            foreach (var grade in grades)
+            for (var index = 0; index < grades.Count; index += 1)
             {
-                result.High = Math.Max(grade, result.High);
-                result.Low = Math.Min(grade, result.Low);
-                result.Average += grade;
-            }
-            
-            result.Average /= grades.Count; 
-            
-            switch (result.Average)
-            {
-                case var d when d >= 90.0:
-                    result.Letter = 'A';
-                    break;
-
-                case var d when d >= 80.0:
-                    result.Letter = 'B';
-                    break;
-
-                case var d when d >= 70.0:
-                    result.Letter = 'C';
-                    break;
-
-                case var d when d >= 60.0:
-                    result.Letter = 'D';
-                    break;
-
-                default:
-                    result.Letter = 'E';
-                    break;
-            }
+                result.Add(grades[index]);
+            }                    
 
             return result;           
         }        
--- src/GradeBook/Statistics.cs
+++ src/GradeBook/Statistics.cs
@@ -1,10 +1,64 @@
+using System;
+
 namespace GradeBook
 {
     public class Statistics
     {
-        public double Average;
+        public double Average
+        {
+            get
+            {
+                return Sum / Count;
+            }
+        }
+
         public double High;
         public double Low;
-        public char Letter;
+        public double Sum;
+
+        public char Letter
+        {
+            get
+            {
+                switch (Average)
+                {
+                    case var d when d >= 90.0:
+                        return 'A';                        
+
+                    case var d when d >= 80.0:
+                        return 'B';                        
+
+                    case var d when d >= 70.0:
+                        return 'C';                        
+
+                    case var d when d >= 60.0:
+                        return 'D';                        
+
+                    default:
+                        return 'E';                        
+                }
+            }
+        }
+
+        public int Count;
+
+        public Statistics()
+        {
+            Sum = 0.0;
+
+            Count = 0;
+
+            High = double.MinValue;
+            Low = double.MaxValue;
+        }
+
+        public void Add(double number)
+        {
+            Sum += number;
+            Count += 1;
+
+            Low = Math.Min(number, Low);
+            High = Math.Max(number, High);
+        }
     }
 }
